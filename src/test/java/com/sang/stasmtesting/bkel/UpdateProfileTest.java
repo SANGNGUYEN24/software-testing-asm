2ed0e97b3189a14e09a00d5ea9ed1d80be748a55
sangnd5 <sangnd5@vng.com.vn>
2022-12-19 21:44:47 +0700

    Add more tests

--- src/test/java/com/sang/stasmtesting/bkel/UpdateProfileTest.java
+++ src/test/java/com/sang/stasmtesting/bkel/UpdateProfileTest.java
@@ -2,6 +2,7 @@ package com.sang.stasmtesting.bkel;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -9,8 +10,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UpdateProfileTest {
@@ -18,6 +27,15 @@ public class UpdateProfileTest {
     public static SelenideElement usernameField;
     public static SelenideElement passwordField;
     public static SelenideElement submitBtn;
+
+    public static SelenideElement profileBtn;
+    public static SelenideElement profileBar;
+    public static SelenideElement editProfileTitle;
+    public static SelenideElement addFileBtn;
+    public static SelenideElement chooseFileBtn;
+    public static SelenideElement uploadChosenFileBtn;
+    public static SelenideElement updateProfileBtn;
+    public static SelenideElement statusBar;
 
     @BeforeAll
     public static void setUpAll() {
@@ -33,6 +51,11 @@ public class UpdateProfileTest {
         passwordField = $("[id='password']");
         submitBtn = $("[class='btn-submit']");
         logInBkel();
+        profileBtn = $("[id='user-menu-toggle']");
+        profileBar = $("[href='https://e-learning.hcmut.edu.vn/user/profile.php']");
+        editProfileTitle = $("[href='https://e-learning.hcmut.edu.vn/user/edit.php?id=85904&returnto=profile']");
+        addFileBtn = $("[class='icon fa fa-file-o fa-fw ']");
+        statusBar = $("[class='alert alert-success alert-block fade in  alert-dismissible']");
     }
 
     @AfterEach
@@ -50,8 +73,72 @@ public class UpdateProfileTest {
         submitBtn.click();
     }
 
+    private void gotoEditPage(){
+        profileBtn.click();
+        profileBar.click();
+        editProfileTitle.click();
+    }
 
+    private void uploadProfilePicture(){
+        SelenideElement chooseFileBtn = $("[id='yui_3_17_2_1_1671455394248_1874']");
+        chooseFileBtn.sendKeys("/assets/avatar.jpg");
 
+        SelenideElement uploadChosenFileBtn = $("[class='fp-upload-btn btn-primary btn']");
+        uploadChosenFileBtn.click();
+    }
 
+    private void clickUpdateProfileBtn(){
+        SelenideElement updateProfileBtn = $("[id='id_submitbutton']");
+        updateProfileBtn.click();
+
+        assertEquals("Đã lưu các thay đổi\n" +
+                "×\n" +
+                "Dismiss this notification", statusBar.getText());
+    }
+
+    // CP-009
+    @Test
+    public void updateAvatarWithoutDeleteOld_success(){
+        gotoEditPage();
+        addFileBtn.click();
+        uploadProfilePicture();
+        clickUpdateProfileBtn();
+    }
+
+    // CP-010
+    @Test
+    public void deleteTheOldPicture_success(){
+        gotoEditPage();
+
+        SelenideElement deleteCheckbox = $("[for='id_deletepicture']");
+        deleteCheckbox.click();
+
+        clickUpdateProfileBtn();
+    }
+
+    @Test
+    public void doNothing_noError(){
+        gotoEditPage();
+        clickUpdateProfileBtn();
+    }
+
+    // CP-011
+    @Test
+    public void updateProfileAndDescription_success() {
+        gotoEditPage();
+
+        SelenideElement chooseFileBtn = $("[id='yui_3_17_2_1_1671455394248_1874']");
+        chooseFileBtn.sendKeys("/assets/avatar.jpg");
+
+        SelenideElement uploadChosenFileBtn = $("[class='uploadChosenFileBtn']");
+        uploadChosenFileBtn.click();
+
+        SelenideElement description = $("[id='id_description_editoreditable']");
+        description.sendKeys("Toi la sinh vien Bach Khoa");
+
+        clickUpdateProfileBtn();
+    }
+
+    //CP-012
 
 }
